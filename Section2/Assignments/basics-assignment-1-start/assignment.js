eeb771f02944526b41902c0533a4317e13c013ed
Fred the Ninja <fredtheninja@icloud.com>
2021-01-27 04:41:57 +0100

    Section Assignment1 Done

---
+++ Section2/Assignments/basics-assignment-1-start/assignment.js
@@ -0,0 +1,12 @@
+let input = 12;
+let result;
+
+//Decided to use log instead of alert, since the popup is annoying
+
+result = 18 + input;
+
+console.log(input);
+console.log((result = result - 8));
+console.log((result = result * 2));
+console.log((result = result / 5));
+console.log((result = result ** 2));
