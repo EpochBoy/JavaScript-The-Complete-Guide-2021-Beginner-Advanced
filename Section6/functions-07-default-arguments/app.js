543825eeaccfed0678858696501be4b758af636b
Fred the Ninja <fredtheninja@icloud.com>
2021-04-17 23:41:37 +0200

    Section 6 Done

--- Section6/functions-07-default-arguments/app.js
+++ Section6/functions-07-default-arguments/app.js
@@ -11,39 +11,33 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 let gameIsRunning = false;
 
 const getPlayerChoice = () => {
-  const selection = prompt(
-    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
-    ''
-  ).toUpperCase();
-  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
-    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-    return;
-  }
-  return selection;
+	const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+	if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+		alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+		return;
+	}
+	return selection;
 };
 
 const getComputerChoice = () => {
-  const randomValue = Math.random();
-  if (randomValue < 0.34) {
-    return ROCK;
-  } else if (randomValue < 0.67) {
-    return PAPER;
-  } else {
-    return SCISSORS;
-  }
+	const randomValue = Math.random();
+	if (randomValue < 0.34) {
+		return ROCK;
+	} else if (randomValue < 0.67) {
+		return PAPER;
+	} else {
+		return SCISSORS;
+	}
 };
 
-const getWinner = (
-  cChoice,
-  pChoice = DEFAULT_USER_CHOICE
-) =>
-  cChoice === pChoice
-    ? RESULT_DRAW
-    : (cChoice === ROCK && pChoice === PAPER) ||
-      (cChoice === PAPER && pChoice === SCISSORS) ||
-      (cChoice === SCISSORS && pChoice === ROCK)
-    ? RESULT_PLAYER_WINS
-    : RESULT_COMPUTER_WINS;
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) =>
+	cChoice === pChoice
+		? RESULT_DRAW
+		: (cChoice === ROCK && pChoice === PAPER) ||
+		  (cChoice === PAPER && pChoice === SCISSORS) ||
+		  (cChoice === SCISSORS && pChoice === ROCK)
+		? RESULT_PLAYER_WINS
+		: RESULT_COMPUTER_WINS;
 
 // if (cChoice === pChoice) {
 //   return RESULT_DRAW;
@@ -58,28 +52,29 @@ const getWinner = (
 // }
 
 startGameBtn.addEventListener('click', () => {
-  if (gameIsRunning) {
-    return;
-  }
-  gameIsRunning = true;
-  console.log('Game is starting...');
-  const playerChoice = getPlayerChoice();
-  const computerChoice = getComputerChoice();
-  let winner;
-  if (playerChoice) {
-    winner = getWinner(computerChoice, playerChoice);
-  } else {
-    winner = getWinner(computerChoice);
-  }
-  let message = `You picked ${playerChoice ||
-    DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefore you `;
-  if (winner === RESULT_DRAW) {
-    message = message + 'had a draw.';
-  } else if (winner === RESULT_PLAYER_WINS) {
-    message = message + 'won.';
-  } else {
-    message = message + 'lost.';
-  }
-  alert(message);
-  gameIsRunning = false;
+	if (gameIsRunning) {
+		return;
+	}
+	gameIsRunning = true;
+	console.log('Game is starting...');
+	const playerChoice = getPlayerChoice();
+	const computerChoice = getComputerChoice();
+	let winner;
+	if (playerChoice) {
+		winner = getWinner(computerChoice, playerChoice);
+	} else {
+		winner = getWinner(computerChoice);
+	}
+	let message = `You picked ${
+		playerChoice || DEFAULT_USER_CHOICE
+	}, computer picked ${computerChoice}, therefore you `;
+	if (winner === RESULT_DRAW) {
+		message = message + 'had a draw.';
+	} else if (winner === RESULT_PLAYER_WINS) {
+		message = message + 'won.';
+	} else {
+		message = message + 'lost.';
+	}
+	alert(message);
+	gameIsRunning = false;
 });
